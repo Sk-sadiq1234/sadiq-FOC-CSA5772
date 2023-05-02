7755b06efb9480177b58498912c7d4bdd661fe39
Sk-sadiq1234 <132328077+Sk-sadiq1234@users.noreply.github.com>
2023-05-02 14:53:47 +0530

    Create Question 13.C

---
+++ Question 13.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, sum = 0;
+
+    printf("Enter the value of N: ");
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i += 2) {
+
+        sum += i * i;
+
+    }
+
+    printf("The sum of the squares of odd numbers is %d", sum);
+
+    return 0;
+
+}
