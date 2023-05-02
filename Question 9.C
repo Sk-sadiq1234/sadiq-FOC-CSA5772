d5284a13a6bff8e7d92e4bff9c4f3eba1f0a302b
Sk-sadiq1234 <132328077+Sk-sadiq1234@users.noreply.github.com>
2023-05-02 14:24:40 +0530

    Create Question 9.C

---
+++ Question 9.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, sum = 0;
+
+    printf("Enter the value of N: ");
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+
+        sum += i * i * 10 + 2;
+
+    }
+
+    printf("The sum of the series is %d", sum);
+
+    return 0;
+
+}
