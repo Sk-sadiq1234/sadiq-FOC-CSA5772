bcea15bb4c8e201752533199cd6b3cdb10338e89
Sk-sadiq1234 <132328077+Sk-sadiq1234@users.noreply.github.com>
2023-05-02 14:22:27 +0530

    Create Question 7.C

---
+++ Question 7.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+   int n, sum=0;
+
+   
+
+   printf("Enter the number of terms in the series: ");
+
+   scanf("%d", &n);
+
+   for (int i = 1; i <= n; i++) {
+
+      if (i % 2 != 0) {
+
+         sum += i;
+
+      }
+
+   }
+
+   printf("The sum of odd numbers in the series is %d", sum);
+
+   return 0;
+
+}
